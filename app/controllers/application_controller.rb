e87313ae49cb912948f2f468d1ce8c8f62dcd209
evaccaro <erv230@nyu.edu>
2017-11-02 11:11:37 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :'new'
+    end
+
+    post '/teams' do
+      @team_info = params
+      erb :team
+    end
 
 end
